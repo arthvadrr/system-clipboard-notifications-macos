c94fccbb52cfbd13e5b138c4d4c63deb531150c0
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2024-01-01 22:12:10 -0600

    Found my way around a weird JIT error

--- scn.swift
+++ scn.swift
@@ -1,9 +1,37 @@
-#!/usr/bin/swift
+import AppKit.NSPasteboard
+import Foundation
+import AVFoundation
 
-import SwiftUI
-import AppKit
+func getClipboardContent() -> String? {
+    let pasteboard: NSPasteboard = NSPasteboard.general
+    return pasteboard.string(forType: .string)
+}
 
-print(NSPasteboard.general.clearContents())
+var previousClipboardContent: String?
+var audioPlayer: AVAudioPlayer?
 
-NotificationCenter.default.addObserver(self, selector: #selector(clipboardChanged),
-                                               name: NSPasteboard.changedNotification, object: nil)
+if let audioFileURL = Bundle.main.url(forResource: "snip_sound", withExtension: "mp3") {
+    do {
+        audioPlayer = try AVAudioPlayer(contentsOf: audioFileURL)
+        audioPlayer?.prepareToPlay()
+    } catch {
+        print("Failed to initialize audio player: \(error)")
+    }
+} else {
+    print("Sound file not found.")
+}
+
+while true {
+    if let currentClipboardContent: String = getClipboardContent() {
+        if currentClipboardContent != previousClipboardContent {
+            previousClipboardContent = currentClipboardContent
+
+            // Ensure audio player is not nil before attempting to play
+            if let player = audioPlayer, !player.isPlaying {
+                player.play()
+            }
+        }
+    }
+
+    sleep(1)
+}
